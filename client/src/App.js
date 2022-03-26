2ee8bcd45673488ac77f8e8a7faae83891d37a41
hussaino03 <m.hussainomer03@gmail.com>
2022-03-26 17:29:17 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-import Home from './components/Home';
+import Home from './components/Home'; // for adding the homepage (navbar)
 
 
 function App() {
