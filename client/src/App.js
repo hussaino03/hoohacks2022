4c8ad004a05ff47057c22be88852974cb617266a
Dan Do <dxd3vmb@virginia.edu>
2022-03-26 16:40:28 -0400

    more homepage stuff

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar"
-import Dogs from "./pages/navbar/Dogs"
+import Home from "./pages/navbar/Home"
 import Cats from "./pages/navbar/Cats"
 import Sheep from "./pages/navbar/Sheep"
 import Goat from "./pages/navbar/Goat"
@@ -13,7 +13,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Dogs />}></Route> {/*This is going to be the homepage*/}
+        <Route path="/" element={<Home />}></Route> {/*This is going to be the homepage*/}
         <Route path='/cats' element={<Cats />} /> {/*user page?*/}.
         <Route path='/sheeps' element={<Sheep />} /> {/*picture page?*/}.
         <Route path='/goats' element={<Goat />} /> {/**/}.
