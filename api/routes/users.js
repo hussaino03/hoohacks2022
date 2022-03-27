598611966c6bec8920eff30542349f523e201915
Austin Houck <akh4rf@virginia.edu>
2022-03-26 20:31:23 -0400

    Login Functionality (back&front)

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var crypto = require('crypto');
 const QueryPromise = require("../database/DBService");
 
 /* GET users listing. */
@@ -14,8 +15,21 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/login', function(req, res, next) {
-  console.log(req.body);
-  res.send("login");
+  let pass = crypto.createHash('sha256').update(req.body.password).digest('hex');
+  QueryPromise("SELECT * FROM Users WHERE username = ? AND password = ? LIMIT 1", [
+    req.body.username,
+    pass,
+  ])
+    .then((results) => {
+      if (results[0]) {res.send({
+        msg: "success",
+        data: results[0],
+      });}
+      else {res.send({"msg": "Incorrect username or password"})}
+    })
+    .catch((err) => {
+      res.send(err);
+    });
 });
 
 module.exports = router;
--- client/src/pages/navbar/Login.jsx
+++ client/src/pages/navbar/Login.jsx
@@ -1,20 +1,38 @@
 import React from "react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
     const [username, setUsername] = useState("");
     const [pass, setPass] = useState("");
+    const nav = useNavigate();
 
     let handleLogin = (e) => {
         e.preventDefault();
         fetch("http://localhost:4000/users/login", {
             method: "POST",
+            headers: {
+                'Content-type': 'application/json'
+            },
             body: JSON.stringify({
                 username: username,
                 password: pass,
             }),
         }).then(res => {
-            console.log(res);
+            res.json().then( results => {
+                if (results['msg'] == 'success') {
+                    // SUCCESSFUL LOGIN
+                    console.log(results);
+                    document.cookie = "userID="+results['data']['userID']+"; path=/;";
+                    nav("/");
+                } else {
+                    // USERNAME OR PASSWORD INCORRECT
+                    console.log(results['msg']);
+                }
+            }
+            ).catch( err =>
+                { console.log(err); }
+            );
         }).catch(err => {
             console.log(err);
         });
