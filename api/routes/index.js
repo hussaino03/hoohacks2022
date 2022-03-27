fceea582c0854a7d6ba539e7b081f150f8921f46
Jack Liu <jackliu612@gmail.com>
2022-03-27 10:25:20 -0400

    twillio (JANK)

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
+const QueryPromise = require("../database/DBService");
+
+const twilio = require('twilio');
+const client = new twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log('hi');
   res.render('index', { title: 'Express' });
+  // client.messages
+  // .create({
+  //   body: 'I really hope this works lol',
+  //   to: '5715240406', // Text this number
+  //   from: '+17626759554', // From a valid Twilio number
+  // })
+  // .then((message) => console.log(message.sid))
+  // .catch((err) => console.log(err));
+});
+
+router.post('/', function(req, res, next) {
+
+  client.messages
+  .create({
+    body: 'I really hope this works lol',
+    to: '5715240406', // Text this number
+    from: '+17626759554', // From a valid Twilio number
+  })
+  .then((message) => console.log(message.sid))
+  .catch((err) => console.log(err));
+
+  QueryPromise("SELECT * FROM Users")
+    .then((results) => {
+      if (results[0]) {res.send({
+        msg: "success",
+        data: results[0],
+      });}
+      else {res.send({"msg": "Incorrect username or password"})}
+    })
+    .catch((err) => {
+      res.send(err);
+    });
 });
 
 module.exports = router;
