58bbf36c8609f514e2d6cb6a2505383b9ec2efce
Jack Liu <jackliu612@gmail.com>
2022-03-27 10:58:59 -0400

    clean up comments

--- api/routes/index.js
+++ api/routes/index.js
@@ -7,16 +7,7 @@ const client = new twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUT
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log('hi');
   res.render('index', { title: 'Express' });
-  // client.messages
-  // .create({
-  //   body: 'I really hope this works lol',
-  //   to: '5715240406', // Text this number
-  //   from: '+17626759554', // From a valid Twilio number
-  // })
-  // .then((message) => console.log(message.sid))
-  // .catch((err) => console.log(err));
 });
 
 router.post('/', function(req, res, next) {
